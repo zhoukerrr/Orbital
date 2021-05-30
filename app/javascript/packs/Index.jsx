1918b2cab32a43771af5cd92b6ef55ade1e99094
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-05-30 11:17:20 +0800

    make it better

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import $, { data } from "jquery";
-import Popper from "popper.js";
+//import $, { data } from "jquery";
+//import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "../components/App";
-import Routes from "../routes/Index";
+//import Routes from "../routes/Index";
 
 document.addEventListener("DOMContentLoaded", () => {
   const data = document.getElementById("helper");
