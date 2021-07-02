429e8d206749c9929d29b9e535ac2c81a7a477cf
Lee Zheng Han <e0544595@u.nus.edu>
2021-07-02 10:44:25 +0800

    Add approved events in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Submitted Events
 20.times do |i|
   Event.create(
     name: "Seed Event #{i + 1}",
@@ -49,6 +43,58 @@ end
     skills: "seed generated skills",
     link: "seed.generated.link",
     contact: "seed generated contact",
+    user_id: 1
+  )
+end
+
+#Approved Events
+20.times do |i|
+  Event.create(
+    name: "Seed Event #{i + 1}",
+    tag: "Environment",
+    summary: "seed generated summary",
+    venue: "seed generated venue",
+    start_date: "2021-06-25T16:00:00.000Z",
+    end_date: "2021-06-26T15:59:59.000Z",
+    details: "seed generated details",
+    skills: "seed generated skills",
+    link: "seed.generated.link",
+    contact: "seed generated contact",
+    status: "approved",
+    user_id: 1
+  )
+end
+
+20.times do |i|
+  Event.create(
+    name: "Seed Event #{i + 21}",
+    tag: "Fund-Raising",
+    summary: "seed generated summary",
+    venue: "seed generated venue",
+    start_date: "2021-06-25T16:00:00.000Z",
+    end_date: "2021-06-26T15:59:59.000Z",
+    details: "seed generated details",
+    skills: "seed generated skills",
+    link: "seed.generated.link",
+    contact: "seed generated contact",
+    status: "approved",
+    user_id: 1
+  )
+end
+
+20.times do |i|
+  Event.create(
+    name: "Seed Event #{i + 41}",
+    tag: "Senior",
+    summary: "seed generated summary",
+    venue: "seed generated venue",
+    start_date: "2021-06-25T16:00:00.000Z",
+    end_date: "2021-06-26T15:59:59.000Z",
+    details: "seed generated details",
+    skills: "seed generated skills",
+    link: "seed.generated.link",
+    contact: "seed generated contact",
+    status: "approved",
     user_id: 1
   )
 end
