2a1cbde545899ab7313b02280e499bbe14a2a8fc
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-07-09 16:11:45 +0800

    Added User Controller access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,34 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
  
   def index
-    @users = User.all
+    if current_user.user_role == "admin"
+      @users = User.all
+    else 
+      @users = User.where(role: "organiser")
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    unless (current_user.user_role == "admin" || @user.role == "organiser")
+      head(404)
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    unless (current_user.user_role == "admin" || @user.id == current_user.id)
+      head(404)
+    end
   end
 
   def update
     respond_to do |format|
       @user = User.find(params[:id])
+      unless (@user.id == current_user.id)
+        head(404)
+        break
+      end
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'Profile updated.' }
       else
@@ -27,6 +41,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :role)
+    params.require(:user).permit(:name)
   end
+
 end
