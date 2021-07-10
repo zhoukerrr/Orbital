49188e90facd18bf8fca6e54619a9dd7e75ef1df
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-07-10 15:03:41 +0800

    Secured registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_before_action :require_no_authentication, :only => []
   #prepend_before_action :authenticate_scope!
+
+  def create
+    if (!user_signed_in? || current_user.user_role == "admin")
+      super
+    else 
+      head(404)
+    end
+  end
 end
