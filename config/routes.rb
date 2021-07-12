41f30e473cc0d176ff62eaea61440fa295855196
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-07-12 17:02:47 +0800

    Removed unused controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       get 'interests/index'
       post 'interests/create'
       delete 'interests/destroy/:id', to: 'interests#destroy'
-      get 'interests/my_interests'
       get 'interests/my_interested_events'
       patch '/attend/:id', to: 'interests#attend'
     end
